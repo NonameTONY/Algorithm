8066daa426e74355e92c537b23f06927c4d59572
Kozin Viacheslav <58078872+NonameTONY@users.noreply.github.com>
2023-03-10 01:23:05 +0300

    Add files via upload
    Практика 3, задача 1Б

---
+++ prog.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void Reverse(int x, int  arr[x]){
+    cout << "Результат:" << endl;
+    x /= 2;
+    for (int i = x-1; i > -1; i--) {
+        cout << arr[i] << " ";
+    }
+    x *= 2;
+    for (int i = x-1; i > x/2-1; i--) {
+        cout << arr[i] << " ";
+    }
+}
+
+int main() {
+
+    srand(time(NULL));
+    int x;
+
+    cout << "Длина массива: ";
+    cin >> x;
+
+    if (x % 2 == 0) {
+        int *arr = new int[x];
+
+        cout << "Исходный массив:" << endl;
+        for(int i = 0; i < x; i++){
+            arr[i] = rand() % 10 + 0;
+            cout << arr[i] << " ";
+        }
+
+        cout << endl;
+
+        Reverse(x, arr);
+            
+        return 0;
+    }
+    else {
+        cout << "В массиве нечетное число элементов!";
+    }
+
+}
