44af6e55be7c6b2f4936272dd60d708b6792c17e
Kozin Viacheslav <58078872+NonameTONY@users.noreply.github.com>
2023-03-10 01:45:40 +0300

    Практика 3, задача 1c

---
+++ prog.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void Result(int x, int  arr[x]){
+    for (int i = 0; i < x; i++){
+        if(arr[i] > 0) cout << arr[i] << " ";
+    }
+    for (int i = 0; i < x; i++){
+        if(arr[i] <= 0) cout << arr[i] << " ";
+    }
+}
+
+int main() {
+
+    srand(time(NULL));
+    int x;
+
+    cout << "Длина массива: ";
+    cin >> x;
+
+    int *arr = new int[x];
+
+    cout << "Исходный массив:" << endl;
+    for(int i = 0; i < x; i++){
+        arr[i] = rand() % 100 + -100;
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Результат:" << endl;
+    Result(x, arr);
+
+    return 0;
+
+}
