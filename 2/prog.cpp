32243dc3fc10065dc98136b7292c016ab2081d50
Kozin Viacheslav <58078872+NonameTONY@users.noreply.github.com>
2023-03-10 04:14:21 +0300

    Add files via upload

---
+++ 2/prog.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main(){
+
+    srand(time(NULL));
+    int x, max = 0, k = 0; /* O(1) */
+
+    cout << "Введите размер массива: "; /* O(1) */
+    cin >> x; /* O(1) */
+
+    int *a = new int[x]; /* O(1) */
+
+    for(int i = 0; i < x; i++){ /* O(x) */
+        a[i] = rand() % 10 + 0;
+    }
+
+    for(int i = 0; i < x; i++){ /* O(x) */
+        if(a[i] > max) max = a[i];
+    }
+
+    for(int i = 0; i < x; i++){ /* O(x) */
+        if(a[i] == max) k++;
+    }
+
+    cout << "Kol-vo max = " << k; /* O(1) */
+
+    return 0;
+
+} /* Временная сложность алгоритма = O(x) */
